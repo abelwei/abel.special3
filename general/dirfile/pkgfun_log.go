4b3148c76e3ba7b907b6f47fb65645900426341d
abelwei <383887138@qq.com>
2021-09-23 10:21:47 +0800

    添加logrus级别设置

--- general/dirfile/pkgfun_log.go
+++ general/dirfile/pkgfun_log.go
@@ -16,8 +16,8 @@ func ConfigLocalFilesystemLogger(logPath string, logFileName string, maxAge time
 	baseLogPaht := path.Join(logPath, logFileName)
 	writer, err := rotatelogs.New(
 		baseLogPaht+".%Y%m%d%H%M",
-		rotatelogs.WithLinkName(baseLogPaht), // 生成软链，指向最新日志文件
-		rotatelogs.WithMaxAge(maxAge), // 文件最大保存时间
+		rotatelogs.WithLinkName(baseLogPaht),      // 生成软链，指向最新日志文件
+		rotatelogs.WithMaxAge(maxAge),             // 文件最大保存时间
 		rotatelogs.WithRotationTime(rotationTime), // 日志切割时间间隔
 	)
 	if err != nil {
@@ -36,7 +36,7 @@ func ConfigLocalFilesystemLogger(logPath string, logFileName string, maxAge time
 
 func SetLogrusCoutColor() {
 	logrus.SetFormatter(&logrus.TextFormatter{
-		ForceColors:true,
+		ForceColors: true,
 	})
 	logrus.SetOutput(ansicolor.NewAnsiColorWriter(os.Stdout))
 }
@@ -45,4 +45,33 @@ func SetLogrusLine() {
 	filenameHook := filename.NewHook()
 	filenameHook.Field = "line"
 	logrus.AddHook(filenameHook)
+}
+
+func SetLogrusLevel(level string) {
+	switch level {
+	case "debug":
+		logrus.SetLevel(logrus.DebugLevel)
+		break
+	case "info":
+		logrus.SetLevel(logrus.InfoLevel)
+		break
+	case "warn":
+		logrus.SetLevel(logrus.WarnLevel)
+		break
+	case "error":
+		logrus.SetLevel(logrus.ErrorLevel)
+		break
+	case "fatal":
+		logrus.SetLevel(logrus.FatalLevel)
+		break
+	case "panic":
+		logrus.SetLevel(logrus.PanicLevel)
+		break
+	case "trace":
+		logrus.SetLevel(logrus.TraceLevel)
+		break
+	default:
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+
 }
